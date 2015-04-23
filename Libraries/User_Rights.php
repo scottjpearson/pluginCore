a68b5ffc0f42c5f82708a171220dd7ba7cd9ab4c
Kyle McGuffin <kyle.mcguffin@vanderbilt.edu>
2015-04-23 16:21:33 -0500

    Add pull of user rights to construction of UserRights object. Add vars to UserRights to store role name, role id, group name and group id.

--- Libraries/User_Rights.php
+++ Libraries/User_Rights.php
@@ -13,6 +13,8 @@ use \Exception;
 
 # Class for looking up and editing a single user's rights on a project
 class User_Rights {
+	public static $SQL_ERROR = 1;
+
 	# Columns
 	public $lock_record;
 	public $lock_record_multiform;
@@ -29,12 +31,56 @@ class User_Rights {
 	public $data_entry;
 	public $record_create;
 	public $participants;
+	public $role_name;
+	public $dag_name;
+	public $role_id;
+	public $group_id;
 	private $project;
 	private $username;
 
 	public function __construct(Project $project, $username) {
 		$this->project = $project;
 		$this->username = $username;
+
+		if($project != NULL && $username != "") {
+			$sql = "SELECT u.expiration, u.role_id, u.group_id, d.group_name, r.role_name,
+						u.lock_record, u.lock_record_multiform, u.data_export_tool, u.data_import_tool, u.data_comparison_tool,
+						u.data_logging, u.file_repository, u.user_rights, u.design, u.data_access_groups, u.reports, u.calendar,
+						u.data_entry, u.record_create, u.participants,
+						r.lock_record AS role_lock_record,  r.lock_record_multiform AS role_lock_record_multiform,  r.data_export_tool AS role_data_export_tool,
+						r.data_import_tool AS role_data_import_tool,  r.data_comparison_tool AS role_data_comparison_tool,
+						r.data_logging AS role_data_logging,  r.file_repository AS role_file_repository,  r.user_rights AS role_user_rights,
+						r.design AS role_design,  r.data_access_groups AS role_data_access_groups,  r.reports AS role_reports,
+						r.calendar AS role_calendar, r.data_entry AS role_data_entry,  r.record_create AS role_record_create,  r.participants AS role_participants
+					FROM redcap_user_rights u
+					LEFT JOIN redcap_data_access_groups d
+						ON (u.group_id = d.group_id
+						AND d.project_id = " . $project->getProjectId() . ")
+					LEFT JOIN redcap_user_roles r
+						ON (u.role_id = r.role_id
+						AND r.project_id = " . $project->getProjectId() . ")
+					WHERE u.project_id = " . $project->getProjectId() . "
+						AND u.username = '$username'";
+
+			$query = db_query($sql);
+			if(!$query) throw new \Exception("Error looking up user rights", self::$SQL_ERROR);
+
+			if($row = db_fetch_assoc($query)) {
+				## Import all variables that are set by role
+				foreach(get_class_vars(get_class($this)) as $varName) {
+					## Variables set by role will have by a standard and role_ version from the above query
+					if(isset($row[$varName]) && isset($row["role_".$varName])) {
+						## Override user_rights table with role version if the person has a role
+						$this->$varName = ($row["role_id"] == "" ? $row[$varName] : $row["role_".$varName]);
+					}
+				}
+
+				$this->role_name = $row["role_name"];
+				$this->dag_name = $row["group_name"];
+				$this->role_id = $row["role_id"];
+				$this->group_id = $row["group_id"];
+			}
+		}
 	}
 
 	public function updateRights($newRights) {
