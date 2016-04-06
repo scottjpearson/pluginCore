89304d6ada000836a3ac3a6d79b49442d865d1a0
Kyle McGuffin <kyle.mcguffin@vanderbilt.edu>
2016-04-05 20:47:22 -0500

    Added check for checkbox values in isVisible function

--- Libraries/MetadataCollection.php
+++ Libraries/MetadataCollection.php
@@ -27,10 +27,11 @@ class MetadataCollection extends \ArrayObject {
 	 * @param $fieldName string
 	 * @param $recordDetails array
 	 */
-	public function isVisible($fieldName, $recordDetails, $exactBranchingLogic = false) {
+	public function isVisible($fieldName, $recordDetails, $exactBranchingLogic = false)
+	{
 		$startTime = microtime(true);
 		/* @var $metadataRow Metadata */
-		if($exactBranchingLogic === false) {
+		if ($exactBranchingLogic === false) {
 			foreach ($this as $metadataRow) {
 				if ($metadataRow->getFieldName() == $fieldName) {
 					$branchingLogic = $metadataRow->getBranchingLogic();
@@ -42,33 +43,38 @@ class MetadataCollection extends \ArrayObject {
 			$branchingLogic = $exactBranchingLogic;
 		}
 
-		if($branchingLogic == "") {
+		if ($branchingLogic == "") {
 			return true;
 		}
-		$newValue = preg_replace_callback("/(\\[)([a-z][a-z|_|0-9]*?)(\\])/",function($matches) use($recordDetails) {
+		$newValue = preg_replace_callback("/(\\[)([a-z][a-z|_|0-9]*?)(\\])/", function ($matches) use ($recordDetails) {
 			return ($recordDetails[$matches[2]] == "" ? "''" : $recordDetails[$matches[2]]);
-		},$branchingLogic);
+		}, $branchingLogic);
+//		$tempValue = $newValue;
+		$newValue = preg_replace_callback("/(\\[)([a-z][a-z|_|0-9]*?)\\(([0-9a-zA-Z]+)\\)(\\])/", function ($matches) use ($recordDetails) {
+			return (in_array($matches[3], $recordDetails[$matches[2]]) ? "1" : "0");
+		}, $newValue);
+
 		//$newValue = str_replace(" = "," == ",$newValue);
-		$newValue = str_replace(" or ",") || (",$newValue);
-		$newValue = str_replace(" and ",") && (",$newValue);
-		$newValue = "(".$newValue.")";
+		$newValue = str_replace(" or ", ") || (", $newValue);
+		$newValue = str_replace(" and ", ") && (", $newValue);
+		$newValue = "(" . $newValue . ")";
 
 		$endTime = microtime(true);
 		self::$time1 += $endTime - $startTime;
 		$startTime = $endTime;
 
-		if(!isset(self::$cachedParses[$newValue])) {
+		if (!isset(self::$cachedParses[$newValue])) {
 			# Create a LogicParser and use it to evaluate our branching logic
 			$parser = new \LogicParser();
-//			echo "Parsing $newValue<br />";
+//			echo "Parsing $tempValue<br />";
+//			echo "Output $newValue<br />";
 			list($logicCode) = $parser->parse($newValue);
 
 			$endTime = microtime(true);
 			self::$time2 += $endTime - $startTime;
 			$startTime = $endTime;
-			self::$cachedParses[$newValue] = call_user_func_array($logicCode,array());
+			self::$cachedParses[$newValue] = call_user_func_array($logicCode, array());
 		}
-
 
 
 		$endTime = microtime(true);
