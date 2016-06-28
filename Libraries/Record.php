09c8b85c45f107e195258a506e6d9565dc2f2749
Kyle McGuffin <kyle.mcguffin@vanderbilt.edu>
2016-06-28 13:55:01 -0500

    Add check on getLabelData to correctly edit "yesno" type fields

--- Libraries/Record.php
+++ Libraries/Record.php
@@ -380,6 +380,9 @@ class Record {
 	# For the given fieldName, returns the label from the element_enum metadata that corresponds to the value stored in the record.
 	# If the fieldName has no element_enum, or the stored value doesn't exist within it, the fieldName's value is returned instead.
 	public function getLabelData($fieldName) {
+		if($this->project->getMetadata($fieldName)->getElementType() == "yesno") {
+			return ($this->getDetails($fieldName) == 1 ? "Yes" : ($this->getDetails($fieldName) === 0 ? "No" : ""));
+		}
 		$labelData = $this->project->renderEnumData($this->getDetails($fieldName),$this->project->getMetadata($fieldName)->getElementEnum());
 		if ($labelData == "") {
 			return $this->getDetails($fieldName);
